5631eae2622556743c77889e35fc46f358ac6783
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-04 10:59:55 -0700

    feat: Implement WallMap.Render and Controller.

--- Raylib/DungeonEditorScreen.cs
+++ Raylib/DungeonEditorScreen.cs
@@ -1,86 +1,67 @@
 namespace CaptainCoder.Dungeoneering.Raylib;
 
-using System.Numerics;
-
 using Raylib_cs;
 
 public class DungeonEditorScreen : IScreen
 {
     public const int CellSize = 16;
     public Cursor Cursor = new(new Position(0, 0), Facing.West);
+    public WallMap WallMap = new();
+
     public void Render()
     {
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.Black);
+        WallMap.Render();
         Cursor.Render();
         Raylib.EndDrawing();
     }
 
     public void HandleUserInput()
     {
-        if (Raylib.IsKeyPressed(KeyboardKey.E))
+        if (Raylib.IsKeyPressed(KeyboardKey.Space))
         {
-            Cursor = Cursor.Rotate();
+            if (WallMap.TryGetWall(Cursor.Position, Cursor.Facing, out _))
+            {
+                WallMap.RemoveWall(Cursor.Position, Cursor.Facing);
+            }
+            else
+            {
+                WallMap.SetWall(Cursor.Position, Cursor.Facing, WallType.Solid);
+            }
         }
-        if (Raylib.IsKeyPressed(KeyboardKey.Q))
-        {
-            Cursor = Cursor.RotateCounterClockwise();
-        }
+        HandleCursorMovement();
+    }
+
+    private void HandleCursorMovement()
+    {
         if (Raylib.IsKeyPressed(KeyboardKey.W))
         {
-            Cursor = Cursor.Move(Facing.North);
+            Cursor = MoveOrRotate(Facing.North);
         }
         if (Raylib.IsKeyPressed(KeyboardKey.D))
         {
-            Cursor = Cursor.Move(Facing.East);
+            Cursor = MoveOrRotate(Facing.East);
         }
         if (Raylib.IsKeyPressed(KeyboardKey.S))
         {
-            Cursor = Cursor.Move(Facing.South);
+            Cursor = MoveOrRotate(Facing.South);
         }
         if (Raylib.IsKeyPressed(KeyboardKey.A))
         {
-            Cursor = Cursor.Move(Facing.West);
+            Cursor = MoveOrRotate(Facing.West);
         }
     }
-}
 
-public static class CursorExtensions
-{
-    public static void Render(this Cursor cursor)
+    private Cursor MoveOrRotate(Facing facing)
     {
-        float x = (float)(cursor.Position.X * DungeonEditorScreen.CellSize);
-        float y = (float)(cursor.Position.Y * DungeonEditorScreen.CellSize);
-        Vector2[] points = CursorPoints(cursor.Facing, x, y);
-        Raylib.DrawTriangle(points[0], points[1], points[2], Color.Yellow);
-    }
-
-    public static Vector2[] CursorPoints(Facing direction, float x, float y)
-    {
-        const int padding = 5;
-        return direction switch
+        if (Cursor.Facing == facing)
         {
-            Facing.North => [
-                new Vector2(x + DungeonEditorScreen.CellSize * 0.5f, y + padding),
-                new Vector2(x + padding, y + DungeonEditorScreen.CellSize - padding),
-                new Vector2(x + DungeonEditorScreen.CellSize - padding, y + DungeonEditorScreen.CellSize - padding)
-            ],
-            Facing.South => [
-                new Vector2(x + padding, y + padding),
-                new Vector2(x + DungeonEditorScreen.CellSize * 0.5f, y + DungeonEditorScreen.CellSize - padding),
-                new Vector2(x + DungeonEditorScreen.CellSize - padding, y + padding),
-            ],
-            Facing.East => [
-                new Vector2(x + padding, y + padding),
-                new Vector2(x + padding, y + DungeonEditorScreen.CellSize - padding),
-                new Vector2(x + DungeonEditorScreen.CellSize - padding, y + DungeonEditorScreen.CellSize * 0.5f),
-            ],
-            Facing.West => [
-                new Vector2(x + DungeonEditorScreen.CellSize - padding, y + padding),
-                new Vector2(x + padding, y + DungeonEditorScreen.CellSize * 0.5f),
-                new Vector2(x + DungeonEditorScreen.CellSize - padding, y + DungeonEditorScreen.CellSize - padding),
-            ],
-            _ => throw new Exception($"Unexpected facing: {direction}"),
-        };
+            return Cursor.Move(facing);
+        }
+        else
+        {
+            return Cursor.Turn(facing);
+        }
     }
 }
