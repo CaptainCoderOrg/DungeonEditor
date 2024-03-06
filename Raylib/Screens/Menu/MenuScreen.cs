1f48b9ef665cc8cb6a0f30ffcebe4366f45fc888
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-06 16:51:37 -0700

    chore: Update Menu to Scroll.

--- Raylib/Screens/Menu/MenuScreen.cs
+++ Raylib/Screens/Menu/MenuScreen.cs
@@ -2,10 +2,13 @@ namespace CaptainCoder.Dungeoneering.Raylib;
 
 using Raylib_cs;
 
-public class MenuScreen(string title, IEnumerable<MenuEntry> items) : IScreen
+public class MenuScreen(string title, IEnumerable<MenuEntry> items, int maxShown = 6) : IScreen
 {
-    const int ItemFontSize = 32;
+    const int SelectedFontSize = 32;
+    const int NotSelectedFontSize = 20;
+    const int Padding = 16;
     const int TitleFontSize = 48;
+    public int MaxItemsShown { get; } = maxShown;
     private readonly MenuEntry[] _items = [.. items];
     private readonly string _title = title;
     private int _selectedIx = 0;
@@ -39,19 +42,27 @@ public class MenuScreen(string title, IEnumerable<MenuEntry> items) : IScreen
 
     public void Render()
     {
-        int menuHeight = TitleFontSize * 2 + ItemFontSize * _items.Length;
+        int menuHeight = NotSelectedFontSize * MaxItemsShown + Padding * 2 + SelectedFontSize + Padding;
         int center = Raylib.GetScreenHeight() / 2;
-        int top = center - menuHeight / 2;
+        int top = center - (menuHeight / 2 + Padding * 3 + TitleFontSize);
         _title.DrawCentered(top, TitleFontSize, Color.White);
 
-        top += TitleFontSize;
-        for (int ix = 0; ix < _items.Length; ix++)
+        top += TitleFontSize + Padding * 3;
+        for (int ix = _selectedIx - 2; ix < _selectedIx + MaxItemsShown; ix++)
         {
-            top += ItemFontSize;
-            Color color = _selectedIx == ix ? Color.Yellow : Color.White;
-            _items[ix].ToString().DrawCentered(top, ItemFontSize, color);
+            if (_selectedIx == ix)
+            {
+                _items[_selectedIx].ToString().DrawCentered(top, SelectedFontSize, Color.Yellow);
+                top += SelectedFontSize + Padding;
+            }
+            else
+            {
+                if (ix >= 0 && ix < _items.Length)
+                {
+                    _items[ix].ToString().DrawCentered(top, NotSelectedFontSize, Color.White);
+                }
+                top += NotSelectedFontSize + Padding;
+            }
         }
     }
-
-
 }
